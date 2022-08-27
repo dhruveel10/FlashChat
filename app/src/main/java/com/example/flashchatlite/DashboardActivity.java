d3c46bce289e0a399aa91e9a2d6bdd3176ef2f16
Chaitya Vohera <chaitya321@gmail.com>
2022-08-28 00:03:09 +0530

    FlashChat Final

---
+++ app/src/main/java/com/example/flashchatlite/DashboardActivity.java
@@ -0,0 +1,88 @@
+package com.example.flashchatlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.flashchatlite.Activities.MainActivity;
+import com.example.flashchatlite.databinding.ActivityDashboardBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    EditText secretCodeBox;
+    Button joinBtn, shareBtn;
+    ActivityDashboardBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        secretCodeBox = findViewById(R.id.codeBox);
+        joinBtn = findViewById(R.id.joinBtn);
+        shareBtn = findViewById(R.id.shareBtn);
+
+        URL serverURL;
+
+
+        try {
+            serverURL = new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions defaultOptions =
+                    new JitsiMeetConferenceOptions.Builder()
+                            .setServerURL(serverURL)
+                            .setWelcomePageEnabled(false)
+                            .build();
+            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(secretCodeBox.getText().toString())
+                        .setWelcomePageEnabled(false)
+                        .build();
+
+                JitsiMeetActivity.launch(DashboardActivity.this, options);
+            }
+        });
+
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected (@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.status:
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(intent, 75);
+                        break;
+                    case R.id.chats:
+                        startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
